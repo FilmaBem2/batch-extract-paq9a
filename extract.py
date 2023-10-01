328ec56f76a04a045c1563e203a0476e17417425
Filma Bem <filmab.emtv2@gmail.com>
2023-10-02 00:37:12 +0100

    Added multithread extraction

--- extract.py
+++ extract.py
@@ -2,6 +2,16 @@
 
 import os
 import subprocess
+import threading
+
+# Functio to extract all the files passed
+
+def extract_file(file_path):
+    
+    command = ["paq9a.exe", "x", file_path]
+    os.system(command)
+
+# Ask the user to input the file directory
 
 source_folder = input("Path to the folder with .paq9a files: ")
 
@@ -9,16 +19,20 @@ source_folder = input("Path to the folder with .paq9a files: ")
 
 file_paths = [os.path.join(source_folder, file) for file in os.listdir(source_folder)]
 
-# Define the command to be executed with 'paq9a.exe x'
-
-command = ["paq9a.exe", "x"]
+thread_list = []
 
 # Loop through each file path and execute the command for each file
 
 for file_path in file_paths:
 
-    command.append(file_path)
-    subprocess.run(command)
-    command.pop() 
+    thread = threading.Thread(target=extract_file, args=(file_path,))
+    thread_list.appends(thread)
+    thread.start()
 
+# Wait for the threads to finish
+
+for thread in thread_list:
+
+    thread.join()
+    
 print("Extraction completed for all files.")
